037c3ebacc484db4fd7e1d3e777237726565a0b1
unknown <you@example.com>
2022-04-19 17:46:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:fares_pro/view/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class  MyApp extends StatelessWidget {
+  const  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: HomeScreen(),
+    );
+  }
+}
+
