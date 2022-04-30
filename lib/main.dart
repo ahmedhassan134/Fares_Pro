ffdb5e43336313a0af20fbdcfc888766d4065c8a
Amr Abd Elhamid <theprofessional.amr@gmail.com>
2022-04-30 03:04:34 +0200

    pdf_viewer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fares_pro/view/screens/Introduction%20to%20communication%20science/Introduction%20to%20communication%20science.dart';
+import 'package:fares_pro/cubit/app_cubit.dart';
 import 'package:fares_pro/view/screens/Sound_disturbances/Sound_disturbances.dart';
 import 'package:fares_pro/view/screens/Speech_and_pain_disorders/Speech_and_pain_disorders.dart';
 import 'package:fares_pro/view/screens/common_questions.dart';
@@ -7,41 +7,43 @@ import 'package:fares_pro/view/screens/home_screen.dart';
 import 'package:fares_pro/view/screens/language_disorders/language_disorders.dart';
 import 'package:fares_pro/view/screens/speech_and_breathing_organ_exercises/speech_and_breathing_organ_exercises.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'view/screens/Introduction to communication science/Introduction to communication science.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class  MyApp extends StatelessWidget {
-  const  MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            fontSize: 24
-          ),
-          elevation: 0
-
-        )
+    return BlocProvider<AppCubit>(
+      create: (context) => AppCubit(),
+      child: MaterialApp(
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+                centerTitle: true,
+                titleTextStyle: TextStyle(fontSize: 24),
+                elevation: 0)),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen(),
+        routes: {
+          HomeScreen.id: (context) => const HomeScreen(),
+          AboutTheApp.id: (context) => const AboutTheApp(),
+          IntroductionToCommunicationScience.id: (context) =>
+              IntroductionToCommunicationScience(),
+          SoundDisturbances.id: (context) => const SoundDisturbances(),
+          SpeechAndPainDisorders.id: (context) =>
+              const SpeechAndPainDisorders(),
+          SpeechAndBreathingOrganExercises.id: (context) =>
+              const SpeechAndBreathingOrganExercises(),
+          LanguageDisorders.id: (context) => const LanguageDisorders(),
+          CommonQuestions.id: (context) => const CommonQuestions()
+        },
       ),
-      debugShowCheckedModeBanner: false,
-
-      home: const HomeScreen(),
-      routes: {
-        HomeScreen.id:(context)=>const HomeScreen(),
-        AboutTheApp.id:(context)=>const AboutTheApp(),
-        IntroductionToCommunicationScience.id:(context)=>const IntroductionToCommunicationScience(),
-        SoundDisturbances.id:(context)=>const SoundDisturbances(),
-        SpeechAndPainDisorders.id:(context)=> const SpeechAndPainDisorders(),
-        SpeechAndBreathingOrganExercises.id:(context)=>const SpeechAndBreathingOrganExercises(),
-        LanguageDisorders.id:(context)=>const LanguageDisorders(),
-        CommonQuestions.id:(context)=>const CommonQuestions()
-      },
     );
   }
 }
-
--- lib/view/screens/Introduction to communication science/Introduction to communication science.dart
+++ lib/view/screens/Introduction to communication science/Introduction to communication science.dart
@@ -1,100 +1,109 @@
+import 'package:fares_pro/cubit/app_cubit.dart';
+import 'package:fares_pro/view/screens/pdf_view_screen/pdf_viewer.dart';
 import 'package:fares_pro/view/widgets/introduction_to_communication_science_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pdfx/pdfx.dart';
 
 import '../../../service/responsive.dart';
-class IntroductionToCommunicationScience extends StatelessWidget {
-  const IntroductionToCommunicationScience({Key? key}) : super(key: key);
-  static String id='introduction';
 
+class IntroductionToCommunicationScience extends StatelessWidget {
+  IntroductionToCommunicationScience({Key? key}) : super(key: key);
+  static String id = 'introduction';
+  final pdfPinchController = PdfController(
+    document: PdfDocument.openAsset('assets/pdfs/book.pdf'),
+  );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: const Text('مقدمة في علم التخاطب ',),
-        elevation: 0,
-      ),
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/one.jpg',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: ' اللغة',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'الكلام',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'الصوت',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'مقومات اكتساب اللغة',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'أسباب تأخر النمو اللغوي',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'اختبارات الحواس',
-                  clr: Colors.blue.shade500,
-
-                ),
-
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'السمع',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'اللغة',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-                  onTap: (){},
-                  text: 'تشخيص حالات تأخر النمو اللغوي ',
-                  clr: Colors.blue.shade500,
-
-                ),
-                IntroductionToCommunicationScienceButton(
-
-                  onTap: (){},
-                  text: 'تصميم خطة لعلاج تأخر النمو اللغوي',
-                  clr: Colors.blue.shade500,
-
-                ),
-                SizedBox(height: SizeConfig.defaultSize *1,)
-
-
-              ],
+    return BlocConsumer<AppCubit, AppState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: const Text(
+              'مقدمة في علم التخاطب ',
             ),
+            elevation: 0,
           ),
-        ],
-      ),
+          body: Stack(
+            children: [
+              Image.asset(
+                'assets/images/one.jpg',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => PdfViewerScreen(
+                                      pdfControllerPinch: pdfPinchController,
+                                    )));
+                      },
+                      text: ' اللغة',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'الكلام',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'الصوت',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'مقومات اكتساب اللغة',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'أسباب تأخر النمو اللغوي',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'اختبارات الحواس',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'السمع',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'اللغة',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'تشخيص حالات تأخر النمو اللغوي ',
+                      clr: Colors.blue.shade500,
+                    ),
+                    IntroductionToCommunicationScienceButton(
+                      onTap: () {},
+                      text: 'تصميم خطة لعلاج تأخر النمو اللغوي',
+                      clr: Colors.blue.shade500,
+                    ),
+                    SizedBox(
+                      height: SizeConfig.defaultSize * 1,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
